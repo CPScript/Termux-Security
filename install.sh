688ede9921ea539fcaab7c006fd2a4f2e362bc19
Disease <83523587+CPScript@users.noreply.github.com>
2024-08-07 09:46:03 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -31,55 +31,16 @@ install_VPNdependencies() {
     done!'
     sleep 2
     clear
-    echo "
-How to run the software:
-'cd src' - Go to dir
-'chmod +x vpn_tui.py' - Make executable
-'vpn_tui.py' - Run file
-    
-    "
-  else
-    echo "Installation cancelled."
-  fi
-}
-
-install_STCdependencies() {
-  if [ ! -f STCrequirements.txt ]; then
-    echo "Error: 'STCrequirements.txt' not found. Please re-install this repository or such file."
-    return 1
-  fi
-
-  dependencies=$(cat STCrequirements.txt)
-  clear # clear everything
-  echo "The following dependencies will be installed:"
-  echo "$dependencies"
-  read -p "Please confirm installation (y/n) " -n 1 -r
-  echo ""
-
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo 'updating!'
-    sleep 2
-    pkg update
-    pkg upgrade
-    echo '
-    installing python'
-    sleep 2
-    pkg install python
-    echo '
-    installing requirements'
-    sleep 2
-    pip install -r STCrequirements.txt
-    sleep 1
-    echo '
-    done!'
-    sleep 2
-    clear
-    echo "
-How to execute the software:
-'cd src' - Go to dir
-'menu.sh' - Execute
-    
-    "
+    echo "Function executed!"
+    read -p "Back to Menu? (y/n): " rmenu
+    if [ "$rmenu" = "y" ]; then
+      clear
+      continue
+    else
+      break
+    fi
+    ;;
+     
   else
     echo "Installation cancelled."
   fi
@@ -88,8 +49,8 @@ How to execute the software:
 banner() { #I got a plan for this banner design later
 echo "
 banner here!
-[1] Install VPN dependencies
-[2] Install Static dependencies
+[1] Install dependencies
+[2] Execute Software
 [3] place holder!
 "
 }
